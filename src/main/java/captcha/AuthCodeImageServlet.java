b9402b5d98547473e5c9fe9dada1bb117f1b6988
user <vichsu38@gmail.com>
2022-05-23 19:37:00 +0800

    web 0523驗整碼繪圖

--- src/main/java/captcha/AuthCodeImageServlet.java
+++ src/main/java/captcha/AuthCodeImageServlet.java
@@ -1,8 +1,14 @@
 package captcha;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
 import java.io.IOException;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,8 +31,32 @@ public class AuthCodeImageServlet extends HttpServlet {
 		req.getSession().setAttribute("authCode", authCode);
         
 		// 產生驗證碼圖片
+		try {             // 畫                       型態   二進回傳
+			ImageIO.write(getAuthCodeImage(authCode),"JPEG",resp.getOutputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 	   // 產生驗證碼圖片方法
+	   // awt 視窗文件 -> BufferedImage 在記憶體中繪圖
+	   private BufferedImage getAuthCodeImage(String authCode) {
+		   // 建立圖像暫存區                      寬  高   印刷Type
+		   BufferedImage img = new BufferedImage(80, 30, BufferedImage.TYPE_INT_RGB);
+	       // 建立畫布
+		   Graphics g = img.getGraphics();
+		   // 設定顏色
+		   g.setColor(Color.yellow);
+		   // 塗滿畫布背景(x,y,x,y) 左上(x,y)右下(x,y)
+		   g.fillRect(0, 0, 80, 30);
+		   // 設定顏色
+		   g.setColor(Color.black);
+		   // 設定字型(字體 , 字樣 , 尺寸)
+		   g.setFont(new Font("新細明體", Font.PLAIN, 30));
+           // 繪文字(呈現字,x,y)
+		   g.drawString(authCode,10,23);
+		   return img;
+	   }
+	
 }

506b3b4f1444ec6cd9e276bc3f451e20fffc18f7
user <vichsu38@gmail.com>
2022-05-30 19:40:22 +0800

    0530

---
+++ src/main/java/jap/entity/Person.java
@@ -0,0 +1,58 @@
+package jap.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Person implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	// unique 唯一
+	// nullable 不可空白
+	@Column(name = "name", length = 50, nullable = false, unique = true)
+	private String name;
+
+	@Column(name = "age" , columnDefinition = "number default 0")
+	private Integer age;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+	
+}

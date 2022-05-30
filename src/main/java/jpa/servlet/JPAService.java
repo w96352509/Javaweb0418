ce578cffb380e0161b35ce4e66b29c653e8d3812
user <vichsu38@gmail.com>
2022-05-30 20:53:23 +0800

    0530

--- src/main/java/jpa/servlet/JPAService.java
+++ src/main/java/jpa/servlet/JPAService.java
@@ -1,9 +1,12 @@
 package jpa.servlet;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
@@ -43,5 +46,34 @@ public class JPAService {
 					etx.commit();
 				}
 	}
+
+	// 單筆
+	public Person getPersonById(Integer id) {
+	  return em.find(Person.class, id);
+	}
+	
+	// 全搜(1)
+	public List<Person> queryAll(){
+		Query query = em.createQuery("select p from Person p");
+		List<Person> list = query.getResultList();
+		return list;
+	}
+	
+	// 全搜(2)
+	public List<Person> queryAll2(){
+		Query query = em.createQuery("from Person p" , Person.class);
+		List<Person> list = query.getResultList();
+		return list;
+	}
+	
+	public List<Person> queryPersonByAge(Integer age){
+		// :... JPQL 語法
+		String sql = "select p from Person p where p.age= :age";
+		Query query = em.createQuery(sql);
+		query.setParameter("age", age); // 對應 :age
+		List<Person> list = query.getResultList();
+	    return list;
+	}
+	
 	
 }
